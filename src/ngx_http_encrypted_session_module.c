419fd27ad0a7f66424dcb297a02af1bad319b554
agentzh (章亦春) <agentzh@gmail.com>
2010-07-05 17:54:05 +0800

    removed C++-style comments.

--- src/ngx_http_encrypted_session_module.c
+++ src/ngx_http_encrypted_session_module.c
@@ -123,16 +123,16 @@ static ngx_http_module_t  ngx_http_encrypted_session_module_ctx = {
 
 ngx_module_t  ngx_http_encrypted_session_module = {
     NGX_MODULE_V1,
-    &ngx_http_encrypted_session_module_ctx,          // module context
-    ngx_http_encrypted_session_commands,             // module directives
-    NGX_HTTP_MODULE,                        // module type
-    NULL,                                   // init master
-    NULL,                                   // init module
-    NULL,                                   // init process
-    NULL,                                   // init thread
-    NULL,                                   // exit thread
-    NULL,                                   // exit process
-    NULL,                                   // exit master
+    &ngx_http_encrypted_session_module_ctx,  /* module context */
+    ngx_http_encrypted_session_commands,     /* module directives */
+    NGX_HTTP_MODULE,                         /* module type */
+    NULL,                                    /* init master */
+    NULL,                                    /* init module */
+    NULL,                                    /* init process */
+    NULL,                                    /* init thread */
+    NULL,                                    /* exit thread */
+    NULL,                                    /* exit process */
+    NULL,                                    /* exit master */
     NGX_MODULE_V1_PADDING
 };
 
