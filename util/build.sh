46e5613e76041b81607790b0a8c22553076d4731
agentzh (章亦春) <agentzh@gmail.com>
2011-08-09 12:27:22 +0800

    confirmed that we work with nginx 1.0.5.

--- util/build.sh
+++ util/build.sh
@@ -4,30 +4,10 @@
 
 root=`pwd`
 home=~
-cd ~/work
 version=$1
-opts=$2
+force=$2
 
-rm -f ~/work/nginx-$version/objs/addon/ndk/ndk.o \
-    ~/work/nginx-$version/objs/addon/ndk-nginx-module/ndk.o
-
-if [ ! -s "nginx-$version.tar.gz" ]; then
-    if [ -f ~/work/nginx-$version.tar.gz ]; then
-        cp ~/work/nginx-$version.tar.gz ./ || exit 1
-    else
-        wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
-    fi
-
-    tar -xzvf nginx-$version.tar.gz || exit 1
-fi
-
-#tar -xzvf nginx-$version.tar.gz || exit 1
-#cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./ || exit 1
-#patch -p0 < nginx-$version-no_pool.patch || exit 1
-
-cd nginx-$version/ || exit 1
-if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
-    ./configure --prefix=/opt/nginx \
+ngx-build $force $version \
             --without-mail_pop3_module \
             --without-mail_imap_module \
             --without-mail_smtp_module \
@@ -38,21 +18,11 @@ if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$
             --without-http_autoindex_module \
             --without-http_auth_basic_module \
             --without-http_userid_module \
+          --add-module=$root/../ndk-nginx-module \
           --add-module=$root/../echo-nginx-module \
           --add-module=$root/../set-misc-nginx-module \
-          --add-module=$root/../ndk-nginx-module \
-          --add-module=$root $opts \
+          --add-module=$root \
           --with-debug
           #--add-module=$home/work/ndk \
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
-
-fi
-if [ -f /opt/nginx/sbin/nginx ]; then
-    rm -f /opt/nginx/sbin/nginx
-fi
-if [ -f /opt/nginx/logs/nginx.pid ]; then
-    kill `cat /opt/nginx/logs/nginx.pid`
-fi
-make -j3
-make install
 
